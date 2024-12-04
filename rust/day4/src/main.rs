4537decb9782081a2eacd91e69a61cb20000f784
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-12-04 17:34:53 +0200

    day4 part2

--- rust/day4/src/main.rs
+++ rust/day4/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 pub const KEY: &str = "XMAS";
+pub const CROSS_MAS: &str = "MAS";
 
 pub enum Direction {
     Horizontal,
@@ -9,8 +10,8 @@ pub enum Direction {
 }
 
 fn main() {
-    println!("Part 1: {}", part_1());
-    // println!("Part 2: {}", part_2());
+    // println!("Part 1: {}", part_1());
+    println!("Part 2: {}", part_2());
 }
 
 fn part_1() -> i32 {
@@ -72,4 +73,56 @@ fn part_1() -> i32 {
     encounters
 }
 
-// fn part_2() -> i32 {}
+fn part_2() -> i32 {
+    let directions = vec![Direction::DiagonalRightUp, Direction::DiagonalRightDown];
+
+    let mut encounters = 0;
+    let puzzle = read_to_string("input")
+        .unwrap()
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    for row in 0..puzzle.len() {
+        for col in 0..puzzle[0].len() {
+            let mut cross = [false, false];
+            for (idx, direction) in directions.iter().enumerate() {
+                let mut key = vec![];
+                if puzzle[row][col] != 'A' {
+                    continue;
+                }
+
+                if !(row > 0 && col > 0 && row + 1 < puzzle.len() && col + 1 < puzzle.len()) {
+                    continue;
+                }
+
+                match direction {
+                    Direction::Horizontal | Direction::Vertical => {
+                        unreachable!()
+                    }
+                    Direction::DiagonalRightUp => {
+                        if row > 0 && col > 0 && row + 1 < puzzle.len() && col + 1 < puzzle.len() {
+                            key.push(puzzle[row - 1][col - 1]);
+                            key.push(puzzle[row + 1][col + 1]);
+                        }
+                    }
+                    Direction::DiagonalRightDown => {
+                        if row > 0 && col > 0 && row + 1 < puzzle.len() && col + 1 < puzzle.len() {
+                            key.push(puzzle[row - 1][col + 1]);
+                            key.push(puzzle[row + 1][col - 1]);
+                        }
+                    }
+                };
+
+                if key.contains(&'M') && key.contains(&'S') {
+                    cross[idx] = true;
+                }
+                if cross.iter().all(|&x| x) {
+                    encounters += 1;
+                }
+            }
+        }
+    }
+
+    encounters
+}
