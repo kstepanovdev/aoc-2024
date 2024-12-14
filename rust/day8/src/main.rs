edf26c24de0502363d98ecead3ec9125e49bb18b
kstepanovdev <k.stepanov@tutanota.com>
2024-12-15 01:39:33 +0200

    day8 p2 && refactor

--- rust/day8/src/main.rs
+++ rust/day8/src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
-use std::ops::{Add, Sub};
+use std::ops::{Add, AddAssign, Sub};
 
 use itertools::Itertools;
 
 fn main() {
     println!("Part 1: {}", part_1());
-    // println!("Part 2: {}", part_2());
+    println!("Part 2: {}", part_2());
 }
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
@@ -20,8 +20,8 @@ impl Location {
         Location { x, y }
     }
 
-    fn check_bounds(&self, map: &Vec<Vec<char>>) -> bool {
-        self.x >= 0 && self.x < map.len() as i32 && self.y >= 0 && self.y < map[0].len() as i32
+    fn check_bounds(&self, state: &PuzzleState) -> bool {
+        self.x >= 0 && self.x < state.width && self.y >= 0 && self.y < state.height
     }
 }
 
@@ -41,30 +41,47 @@ impl Add for Location {
     }
 }
 
-fn part_1() -> i32 {
-    let mut map = vec![];
-    let mut locations: HashMap<char, HashSet<Location>> = HashMap::new();
-    let mut resonanses = HashSet::new();
+impl AddAssign for Location {
+    fn add_assign(&mut self, other: Location) {
+        *self = Location::new(self.x + other.x, self.y + other.y);
+    }
+}
+
+fn read_input() -> PuzzleState {
+    let mut state = PuzzleState::default();
 
     for (x, line) in read_to_string("input").unwrap().lines().enumerate() {
         if line.is_empty() {
             continue;
         }
 
-        map.push(vec![]);
+        state.width = line.len() as i32;
         for (y, c) in line.trim().chars().enumerate() {
-            map[x].push(c);
-
             if c != '.' {
-                locations
+                state
+                    .locations
                     .entry(c)
                     .or_default()
                     .insert(Location::new(x as i32, y as i32));
             }
         }
+        state.height = (x + 1) as i32;
     }
+    state
+}
 
-    for (_, positions) in locations {
+#[derive(Debug, Default, Clone)]
+pub struct PuzzleState {
+    height: i32,
+    width: i32,
+    locations: HashMap<char, HashSet<Location>>,
+    resonanses: HashSet<Location>,
+}
+
+fn part_1() -> i32 {
+    let mut state = read_input();
+
+    for (_, positions) in state.locations.iter() {
         for pairs in positions.iter().combinations(2) {
             let location = *pairs[0];
             let neighbour = *pairs[1];
@@ -72,14 +89,41 @@ fn part_1() -> i32 {
             let resonance_1 = location + (location - neighbour);
             let resonance_2 = neighbour + (neighbour - location);
 
-            if resonance_1.check_bounds(&map) {
-                resonanses.insert(resonance_1);
+            if resonance_1.check_bounds(&state) {
+                state.resonanses.insert(resonance_1);
             };
-            if resonance_2.check_bounds(&map) {
-                resonanses.insert(resonance_2);
+            if resonance_2.check_bounds(&state) {
+                state.resonanses.insert(resonance_2);
             };
         }
     }
 
-    resonanses.len() as i32
+    state.resonanses.len() as i32
+}
+
+fn part_2() -> i32 {
+    let mut state = read_input();
+
+    for (_, positions) in state.locations.iter() {
+        for pairs in positions.iter().combinations(2) {
+            let location = *pairs[0];
+            let neighbour = *pairs[1];
+
+            let resonanse_1_diff = location - neighbour;
+            let resonanse_2_diff = neighbour - location;
+
+            let mut resonance_1 = location;
+            while resonance_1.check_bounds(&state) {
+                state.resonanses.insert(resonance_1);
+                resonance_1 += resonanse_1_diff;
+            }
+            let mut resonance_1 = location;
+            while resonance_1.check_bounds(&state) {
+                state.resonanses.insert(resonance_1);
+                resonance_1 += resonanse_2_diff;
+            }
+        }
+    }
+
+    state.resonanses.len() as i32
 }
